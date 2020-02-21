b20a0612d0e188565366996c4f99d56dac787962
Fritask <fritaske@gmail.com>
2020-02-21 09:00:50 -0300

    Atualização dos arquivos de Ícones de categoria

--- GetBusinessUnity.js
+++ GetBusinessUnity.js
@@ -1,5 +1,6 @@
 $(document).ready(function()
 {
+    var IGBusinessUnityUser;
     function IGGetCookie(cname) {
         var name = cname + "=";
         var decodedCookie = decodeURIComponent(document.cookie);
@@ -40,42 +41,42 @@ $(document).ready(function()
     // document.getElementById("IGTesteUserInfo").innerText = IGBusinessUnityUser;
     // document.getElementById("IGPecaBanner1").src = "http://b.martins.com.br/Conteudo/Banner/EmailMarketing/banner1-" + IGEstadoCliente + "-" + IGBusinessUnityUser;
 
-    // alert(IGBusinessUnityUser);
+    console.log(IGBusinessUnityUser);
 
-    if (IGBusinessUnityUser == 1)
-    {
-        alert("Varejo Alimentar");
-    }
-    else if (IGBusinessUnityUser == 2)
-    {
-        alert("Eletro");
-    }
-    else if (IGBusinessUnityUser == 3)
-    {
-        alert("Martcon");
-    }
-    else if (IGBusinessUnityUser == 4)
-    {
-        alert("Farma Nacional");
-    }
-    else if (IGBusinessUnityUser == 5)
-    {
-        alert("Informática");
-    }
-    else if (IGBusinessUnityUser == 7)
-    {
-        alert("Atacado");
-    }
-    else if (IGBusinessUnityUser == 8)
-    {
-        alert("Pessoa Física");
-    }
-    else if (IGBusinessUnityUser == 9)
-    {
-        alert("Veterinário");
-    }
-    else if (IGBusinessUnityUser == 10)
-    {
-        alert("Smart");
-    }
+    // if (IGBusinessUnityUser == 1)
+    // {
+    //     alert("Varejo Alimentar");
+    // }
+    // else if (IGBusinessUnityUser == 2)
+    // {
+    //     alert("Eletro");
+    // }
+    // else if (IGBusinessUnityUser == 3)
+    // {
+    //     alert("Martcon");
+    // }
+    // else if (IGBusinessUnityUser == 4)
+    // {
+    //     alert("Farma Nacional");
+    // }
+    // else if (IGBusinessUnityUser == 5)
+    // {
+    //     alert("Informática");
+    // }
+    // else if (IGBusinessUnityUser == 7)
+    // {
+    //     alert("Atacado");
+    // }
+    // else if (IGBusinessUnityUser == 8)
+    // {
+    //     alert("Pessoa Física");
+    // }
+    // else if (IGBusinessUnityUser == 9)
+    // {
+    //     alert("Veterinário");
+    // }
+    // else if (IGBusinessUnityUser == 10)
+    // {
+    //     alert("Smart");
+    // }
 });
