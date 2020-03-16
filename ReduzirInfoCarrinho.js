5722412e03474f8cd475c7c419e5e9e2a4c0c602
Fritask <fritas.series@gmail.com>
2020-03-16 17:48:30 -0300

    Código experimento Reduzir Informações do Carrinho

---
+++ ReduzirInfoCarrinho.js
@@ -0,0 +1,15 @@
+$(document).ready(function()
+{
+    $('.cods').css('display', 'none');
+    $('.js-voucher-respond').css('display', 'none');
+    $('<a style="font-size: 16px; font-weight: bold; text-decoration: none; padding: 3px; border: 1px solid #23527C;" class="IGToggleCupomDesconto" href="javascript:void(0)"><i class="fas fa-ticket-alt"></i> Possui cupom de desconto?</a>').insertBefore('.js-voucher-respond');
+    $('.IGToggleCupomDesconto').each(function( i ){
+        $(this).click(function(){
+            $(this).next().toggle();
+        })
+    }).mouseover(function(){
+        $(this).css('background-color', '#23527C').css('color', '#ffffff');
+    }).mouseout(function(){
+        $(this).css('background-color', '#ffffff').css('color', '#23527C');
+    });
+});
