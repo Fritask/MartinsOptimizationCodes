4554c4b7f9d9dda0f15dda34b436fa501b62a11c
Fritask <fritas.series@gmail.com>
2020-03-18 18:13:58 -0300

    Evolução na lógica do experimento 31

--- OtimizacaoAddItensAoCarrinho.js
+++ OtimizacaoAddItensAoCarrinho.js
@@ -1,48 +1,32 @@
 $(document).ready(function()
 {
-    $(".product").each(function( i ){
-        // $(this).$('div').$('form').attr('id');
-        // $(this).$('div').$('form').$('div.action-to-buy').$('div button').click();
-        $(this).append('<a class="IGAddToCartButton" href="javascript:void(0)" style="padding: 12px; background-color: #004282; color: #ffffff; position: absolute; bottom: 0; right: 0"><i class="fas fa-shopping-cart"></i></a>');
-        $(this).find('.IGAddToCartButton').click(function(){
-            // $(this).find('div').find('form').find('div.action-to-buy').find('div button').click();
-            $(this).find('div').find('form').find('div.action-to-buy').find('div').find('button').css('background-color', 'red');
+    var IGDivCLassDispoProds = document.getElementsByClassName("products")[0];
+
+    if(!(IGDivCLassDispoProds.classList.contains("listNoImage")) && !(IGDivCLassDispoProds.classList.contains("list")))
+    {
+        $(".product").each(function( i ){
+            var IGCTACardPLP = $(this).children('div').children('form').children('div.action-to-buy').children('div').children('button');
+
+            if(IGCTACardPLP.text() == " COMPRAR")
+            {
+                $(this).append('<a class="IGAddToCartButton" href="javascript:void(0)" style="width: 51.75px; height: 53px; background-color: #004282; color: #ffffff; position: absolute; bottom: 0; right: 0"><i style="padding-top: 19px;" class="fas fa-shopping-cart"></i></a>');
+                IGCTACardPLP.css('visibility', 'hidden');
+                $(this).find('.IGAddToCartButton').click(function(){
+                    IGCTACardPLP.click();
+                }).mouseover(function( event ){
+                    $(this).stop().css('background-color', '#F27934').animate({
+                        width: "100%"
+                    }, 200, function(){
+                        $(this).children().replaceWith('<i style="padding-top: 19px;" class="fas fa-shopping-cart"><p style="font-family: SegoeUI, sans-serif; display: inline"> ADICIONAR AO CARRINHO</p></i>');
+                    });
+                }).mouseout(function( event ){
+                    $(this).stop().css('background-color', '#004282').animate({
+                        width: "51.75px"
+                    }, 200, function(){
+                        $(this).children().replaceWith('<i style="padding-top: 19px;" class="fas fa-shopping-cart"></i>');
+                    });
+                });
+            }
         });
-    });
-
-
-
-
-    
-
-    var x9884787252 = document.getElementsByClassName('boxButton');
-    debugger;
-    console.log(x9884787252);
-    // $('.icon-lorry').css('display', 'none');
-    $('div.boxButton button').css('visibility', 'hidden');
-    // $('div.boxButton button').css('visibility', 'hidden').css('width', '0').css('height', '0').css('margin', '0').css('padding', '0');
-    $('div.boxButton button i').css('visibility', 'visible');
-
-    // $('div.boxButton button').text('').append('<i class="fas fa-shopping-cart"></i>');
-    // $('.cods').css('display', 'none');
-    // $('.js-voucher-respond').css('display', 'none');
-    // $('<a style="font-size: 16px; font-weight: bold; text-decoration: none; padding: 3px; border: 1px solid #23527C;" class="IGToggleCupomDesconto" href="javascript:void(0)"><i class="fas fa-ticket-alt"></i> Possui cupom de desconto?</a>').insertBefore('.js-voucher-respond');
-    // $('.IGToggleCupomDesconto').each(function( i ){
-    //     $(this).click(function(){
-    //         $(this).next().toggle();
-    //     })
-    // }).mouseover(function(){
-    //     $(this).css('background-color', '#23527C').css('color', '#ffffff');
-    // }).mouseout(function(){
-    //     $(this).css('background-color', '#ffffff').css('color', '#23527C');
-    // });
-    $('#IGTESTETESTE22223254').click(function(){
-        var teste5665161 = document.getElementsByClassName("boxButton");
-        $("div.boxButton button").first().click();
-    })
+    }
 });
-
-<button onclick="document.getElementsByClassName('boxButton')[3].click()">aaaaaa</button>
-<button onclick="document.getElementsByClassName('boxButton')[3].click()">aaaaaa</button>
-
-add-to-cart--martins_1601448
