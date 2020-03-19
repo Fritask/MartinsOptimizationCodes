e3a05605467f1843f6ee0a3f10d8baf00773dc84
Fritask <fritas.series@gmail.com>
2020-03-19 16:49:15 -0300

    Melhorias nos códigos dos experimentos 28 e 31

--- OtimizacaoAddItensAoCarrinho.js
+++ OtimizacaoAddItensAoCarrinho.js
@@ -2,31 +2,47 @@ $(document).ready(function()
 {
     var IGDivCLassDispoProds = document.getElementsByClassName("products")[0];
 
-    if(!(IGDivCLassDispoProds.classList.contains("listNoImage")) && !(IGDivCLassDispoProds.classList.contains("list")))
+    IGOtimizacaoAddItensCarinho = function()
     {
-        $(".product").each(function( i ){
-            var IGCTACardPLP = $(this).children('div').children('form').children('div.action-to-buy').children('div').children('button');
 
-            if(IGCTACardPLP.text() == " COMPRAR")
-            {
-                $(this).append('<a class="IGAddToCartButton" href="javascript:void(0)" style="width: 51.75px; height: 53px; background-color: #004282; color: #ffffff; position: absolute; bottom: 0; right: 0"><i style="padding-top: 19px;" class="fas fa-shopping-cart"></i></a>');
-                IGCTACardPLP.css('visibility', 'hidden');
-                $(this).find('.IGAddToCartButton').click(function(){
-                    IGCTACardPLP.click();
-                }).mouseover(function( event ){
-                    $(this).stop().css('background-color', '#F27934').animate({
-                        width: "100%"
-                    }, 200, function(){
-                        $(this).children().replaceWith('<i style="padding-top: 19px;" class="fas fa-shopping-cart"><p style="font-family: SegoeUI, sans-serif; display: inline"> ADICIONAR AO CARRINHO</p></i>');
-                    });
-                }).mouseout(function( event ){
-                    $(this).stop().css('background-color', '#004282').animate({
-                        width: "51.75px"
-                    }, 200, function(){
-                        $(this).children().replaceWith('<i style="padding-top: 19px;" class="fas fa-shopping-cart"></i>');
-                    });
-                });
-            }
-        });
-    }
+        if(!(IGDivCLassDispoProds.classList.contains("listNoImage")) && !(IGDivCLassDispoProds.classList.contains("list")))
+        {
+            $(".product").each(function( i ){
+
+                var IGCTACardPLP = $(this).children('div').children('form').children('div.action-to-buy').children('div').children('button');
+                if(IGCTACardPLP.is(":visible"))
+                {
+                    if(IGCTACardPLP.text() == " COMPRAR")
+                    {
+                        $(this).append('<a class="IGAddToCartButton" href="javascript:void(0)" style="width: 51.75px; height: 45px; background-color: #004282; color: #ffffff; position: absolute; bottom: 0; right: 0"><i style="padding-top: 15px;" class="fas fa-shopping-cart"></i></a>');
+                        IGCTACardPLP.css('visibility', 'hidden');
+                        $(this).find('.IGAddToCartButton').click(function(){
+                            IGCTACardPLP.click();
+                        }).mouseover(function( event ){
+                            $(this).stop().css('background-color', '#F27934').animate({
+                                width: "100%"
+                            }, 200, function(){
+                                $(this).children().replaceWith('<i style="padding-top: 15px;" class="fas fa-shopping-cart"><p style="font-family: SegoeUI, sans-serif; display: inline"> ADICIONAR AO CARRINHO</p></i>');
+                            });
+                        }).mouseout(function( event ){
+                            $(this).stop().css('background-color', '#004282').animate({
+                                width: "51.75px"
+                            }, 200, function(){
+                                $(this).children().replaceWith('<i style="padding-top: 15px;" class="fas fa-shopping-cart"></i>');
+                            });
+                        });
+                    }
+                }
+            });
+        }
+
+    };
+    IGOtimizacaoAddItensCarinho();
+
+    $("#xtt-load-more-button").click(function(){
+        setTimeout(function(){
+            IGOtimizacaoAddItensCarinho();
+        }, 4400);
+    });
+
 });
--- ReduzirInfoCarrinho.js
+++ ReduzirInfoCarrinho.js
@@ -1,7 +1,14 @@
 $(document).ready(function()
 {
     $('.cods').css('display', 'none');
-    $('.js-voucher-respond').css('display', 'none');
+
+    $('.js-voucher-validation-container').each(function( i ){
+        if(!($(this).children().length > 0))
+        {
+            $(this).parent().css('display', 'none');
+        }
+    });
+
     $('<a style="font-size: 16px; font-weight: bold; text-decoration: none; padding: 3px; border: 1px solid #23527C;" class="IGToggleCupomDesconto" href="javascript:void(0)"><i class="fas fa-ticket-alt"></i> Possui cupom de desconto?</a>').insertBefore('.js-voucher-respond');
     $('.IGToggleCupomDesconto').each(function( i ){
         $(this).click(function(){
